c3baaa6c4f08366f037be1aee2654baeb1a424af
Shale Craig <shalecraig@gmail.com>
2012-06-03 03:26:42 -0400

    bumped up a version - made it more professional

--- tynt_Stopper.js
+++ tynt_Stopper.js
@@ -1,6 +1,8 @@
 (function () {
 	var Tynt = window.Tynt || [];
 	if (typeof Tynt.TIL === "undefined") {
-		Tynt.TIL = "nope, not running today!";
+        Tynt.TIL = "foo bar";
+		Tynt.status = "Tynt stopped";
+        Tynt.version = "v 1.0.4";
 	}
 })();
